6fd56e078c639852b0756d487ef0ee6ea4583e33
Jorge Martin <jorge.martin@pulselive.com>
2022-09-02 14:01:00 +0200

    Tweaked SearchBar composable's styling

--- app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -21,12 +21,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.TextField
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basiclayouts.ui.theme.MySootheTheme
@@ -46,6 +51,14 @@ fun SearchBar(
     TextField(
         value = "",
         onValueChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Filled.Search,
+                contentDescription = stringResource(id = R.string.placeholder_search)
+            )
+        },
+        colors = TextFieldDefaults.textFieldColors(backgroundColor = MaterialTheme.colors.surface),
+        placeholder = { Text(text = stringResource(id = R.string.placeholder_search)) },
         modifier = modifier
             .fillMaxWidth()
             .heightIn(min = 56.dp)
